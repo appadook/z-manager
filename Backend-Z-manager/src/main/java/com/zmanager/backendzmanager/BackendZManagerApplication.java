66e31f9415150870de792e4c3809a6a5ebdf6013
Kurtik Appadoo <kurtik@Kurtiks-MacBook-Air.local>
2024-08-16 15:21:47 -0400

    initial commit

---
+++ Backend-Z-manager/src/main/java/com/zmanager/backendzmanager/BackendZManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zmanager.backendzmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendZManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendZManagerApplication.class, args);
+    }
+
+}
---
+++ Backend-Z-manager/src/test/java/com/zmanager/backendzmanager/BackendZManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmanager.backendzmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendZManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

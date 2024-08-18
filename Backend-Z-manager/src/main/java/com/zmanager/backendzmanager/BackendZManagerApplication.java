da3eb7707d8854c6a6eb6be06f8f1310c60042e5
Kurtik Appadoo <kurtik@kurtiks-air.lan>
2024-08-18 15:12:02 -0400

    added users table and post mapping

--- Backend-Z-manager/src/main/java/com/zmanager/backendzmanager/BackendZManagerApplication.java
+++ Backend-Z-manager/src/main/java/com/zmanager/backendzmanager/BackendZManagerApplication.java
@@ -1,5 +1,6 @@
 package com.zmanager.backendzmanager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendZManagerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("/Users/kurtik/Projects/Z_manager_web_app") // Set this to your project root
+                .load();
+
+        // Set environment variables
+        System.setProperty("SUPABASE_DB_URL", dotenv.get("SUPABASE_DB_URL"));
+        System.setProperty("SUPABASE_DB_USERNAME", dotenv.get("SUPABASE_DB_USERNAME"));
+        System.setProperty("SUPABASE_DB_PASSWORD", dotenv.get("SUPABASE_DB_PASSWORD"));
+
         SpringApplication.run(BackendZManagerApplication.class, args);
     }
-
 }
---
+++ Backend-Z-manager/src/main/java/com/zmanager/backendzmanager/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.zmanager.backendzmanager.controller;
+
+import com.zmanager.backendzmanager.model.User;
+import com.zmanager.backendzmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+}
